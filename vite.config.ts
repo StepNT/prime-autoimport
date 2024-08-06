111aa5b582407f1451fa456af22f64f68d0b46ca
Jutipong <techcom.ec20@gmail.com>
2024-08-06 14:18:30 +0700

    chore: Update dependencies and import PrimeVue components
    Update dependencies in package.json to the latest versions of Vue, PrimeVue, and TypeScript. Import and configure PrimeVue components and services in main.ts.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite';
+import { PrimeVueResolver } from '@primevue/auto-import-resolver';
+import { fileURLToPath } from 'node:url';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    Components({
+      resolvers: [PrimeVueResolver()]
+    })
+  ],
+  resolve: {
+    alias: {
+      '@App': fileURLToPath(new URL('./src/App.vue', import.meta.url)),
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@core': fileURLToPath(new URL('./src/@core', import.meta.url)),
+      '@assets': fileURLToPath(new URL('./assets', import.meta.url)),
+      '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
+      '@images': fileURLToPath(new URL('./src/assets/images', import.meta.url)),
+      '@utils': fileURLToPath(new URL('./src/utils', import.meta.url)),
+      '@types': fileURLToPath(new URL('./src/types', import.meta.url)),
+      '@stores': fileURLToPath(new URL('./src/stores', import.meta.url)),
+    },
+  }
 })
