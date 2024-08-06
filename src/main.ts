1d88b388eafcfb6cb0b8fe20c457953186d30ee0
Jutipong <techcom.ec20@gmail.com>
2024-08-06 14:03:39 +0700

    chore: Update Vue and PrimeVue dependencies
    Update the Vue and PrimeVue dependencies in the package.json file to the latest versions. This includes updating Vue to version 3.4.35 and adding the PrimeVue dependencies with version 4.0.4. Additionally, update the devDependencies to use the latest versions of the Vite plugin for Vue, TypeScript, Vite, and vue-tsc.
    
    Update main.ts to import and configure PrimeVue components and services.
    
    Note: This commit message follows the conventional commit format, starting with a type (chore) followed by a brief description of the changes.

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,34 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+// import { createApp } from 'vue'
+// import './style.css'
+// import App from './App.vue'
 
-createApp(App).mount('#app')
+// createApp(App).mount('#app')
+
+
+import { createApp } from 'vue';
+import App from './App.vue';
+// import router from './router';
+
+import Aura from '@primevue/themes/aura';
+import PrimeVue from 'primevue/config';
+import ConfirmationService from 'primevue/confirmationservice';
+import ToastService from 'primevue/toastservice';
+
+import '@/assets/styles.scss';
+import '@/assets/tailwind.css';
+
+const app = createApp(App);
+
+// app.use(router);
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura,
+        options: {
+            darkModeSelector: '.app-dark'
+        }
+    }
+});
+app.use(ToastService);
+app.use(ConfirmationService);
+
+app.mount('#app');
