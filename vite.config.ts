587f46386b0e99c8450217ce0c35eeaa9c1fa1cb
jutipong subin <T660046@tcrbank.com>
2024-08-22 16:17:06 +0700

    chore: Update npm dependencies and devDependencies

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite'
 import { PrimeVueResolver } from '@primevue/auto-import-resolver'
 import UnoCSS from 'unocss/vite'
+import ViteFonts from 'unplugin-fonts/vite'
 
 export default defineConfig({
     plugins: [
@@ -12,10 +13,19 @@ export default defineConfig({
         Components({
             resolvers: [PrimeVueResolver()],
         }),
+        ViteFonts({
+            google: {
+                families: [
+                    {
+                        name: 'Inter',
+                        styles: 'wght@100;300;400;500;700;900',
+                    },
+                ],
+            },
+        }),
     ],
     resolve: {
         alias: {
-            '@App': fileURLToPath(new URL('./src/App.vue', import.meta.url)),
             '@': fileURLToPath(new URL('./src', import.meta.url)),
             '@core': fileURLToPath(new URL('./src/@core', import.meta.url)),
             '@assets': fileURLToPath(new URL('./assets', import.meta.url)),
