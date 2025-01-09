c7bc8b20bc3af5488418f329121154ef16910284
T660046@MacboookPro <T660046@tcrbank.com>
2025-01-09 17:47:37 +0700

    feat: Update brand label and input layout for improved responsiveness in filter form

--- src/components/product/filter-form-horizontal.vue
+++ src/components/product/filter-form-horizontal.vue
@@ -13,20 +13,20 @@
         <template #content>
             <div class="grid gap-4 md:grid-cols-3 sm:flex-row">
                 <div class="input-group">
-                    <label class="col-span-3">Brand</label>
-                    <div class="col-span-9">
+                    <label class="col-span-12 md:col-span-3">Brand</label>
+                    <div class="col-span-12 md:col-span-9">
                         <InputText type="text" w-full />
                     </div>
                 </div>
                 <div class="input-group">
-                    <label class="col-span-3">Brand</label>
-                    <div class="col-span-9">
+                    <label class="col-span-12 md:col-span-3">Brand</label>
+                    <div class="col-span-12 md:col-span-9">
                         <InputText type="text" w-full />
                     </div>
                 </div>
                 <div class="input-group">
-                    <label class="col-span-3">Brand</label>
-                    <div class="col-span-9">
+                    <label class="col-span-12 md:col-span-3">Brand</label>
+                    <div class="col-span-12 md:col-span-9">
                         <InputText type="text" w-full />
                     </div>
                 </div>
