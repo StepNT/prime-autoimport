d4878cf985e4a78c0a4c158d36485b15909ed711
T660046@MacboookPro <T660046@tcrbank.com>
2025-01-10 13:32:34 +0700

    feat: Enhance horizontal filter form with new state management and improved input structure

--- src/components/product/filter-form-horizontal.vue
+++ src/components/product/filter-form-horizontal.vue
@@ -1,5 +1,16 @@
 <script lang="ts" setup>
-
+const state = reactive({
+    master: {
+        status: [
+            { id: true, text: 'Active' },
+            { id: false, text: 'InActive' },
+        ],
+    },
+    search: {
+        // autoCompleteServer: '0d464588-56a6-4831-b085-f26cb65ee7fd',
+        // autoCompleteMultipleServer: ['0d464588-56a6-4831-b085-f26cb65ee7fd', 'd85f5eb3-4b9c-4483-9b77-29010b0a93a3', 'dc79347f-90b2-4ffb-9140-69e54648e180'],
+    } as Product,
+})
 </script>
 
 <template>
@@ -11,23 +22,31 @@
         </template>
 
         <template #content>
-            <div class="grid flex-row gap-4 md:grid-cols-3">
-                <div class="input-group">
-                    <label class="col-span-12 md:col-span-3">Brand</label>
-                    <div class="col-span-12 md:col-span-9">
-                        <InputText type="text" />
+            <div row-3>
+                <div input-group>
+                    <label input-group-col-3>Brand</label>
+                    <div input-group-col-9>
+                        <InputText v-model="state.search.brand" type="text" />
+                    </div>
+                </div>
+                <div input-group>
+                    <label input-group-col-3>Title</label>
+                    <div input-group-col-9>
+                        <InputText v-model="state.search.title" type="text" />
                     </div>
                 </div>
                 <div class="input-group">
-                    <label class="col-span-12 md:col-span-3">Brand</label>
-                    <div class="col-span-12 md:col-span-9">
-                        <InputText type="text" />
-                    </div>
-                </div>
-                <div class="input-group">
-                    <label class="col-span-12 md:col-span-3">Brand</label>
-                    <div class="col-span-12 md:col-span-9">
-                        <InputText type="text" />
+                    <label input-group-col-3>Status</label>
+                    <div input-group-col-9>
+                        <Select
+                            v-model="state.search.status"
+                            :options="state.master.status"
+                            option-label="text"
+                            placeholder="Select a Status"
+                            :show-clear="true"
+                            :filter="true"
+                            :auto-filter-focus="true"
+                        />
                     </div>
                 </div>
             </div>
--- uno.config.ts
+++ uno.config.ts
@@ -1,35 +1,14 @@
 import { defineConfig, presetAttributify, presetIcons, presetUno } from 'unocss'
 
 export default defineConfig({
-    shortcuts: {
-        // 'grid': 'flex flex-wrap',
-        // 'btn-footer': 'flex flex-wrap mt-0rem mb-0rem mr-1rem ml-1rem justify-end gap-1',
-        // 'row': 'flex flex-wrap mr-0 ml-0 mt--0.5rem mb--1.25rem',
-        // 'row': 'flex flex-wrap',
-        // 'row': 'flex flex-wrap',
-        //
-        'input-group': 'grid-cols-12 grid gap-2 items-baseline [&_label]:md:text-right [&_input]:w-full',
-        // t
-    },
-    rules: [
-        // [/^m-(\d+)$/, ([, d]) => ({ margin: `${Number(d) / 4}rem` })],
-        // [/^p-(\d+)$/, match => ({ padding: `${Number(match[1]) / 4}rem` })],
-
-        // [/^col$/, () => ({ flexGrow: 1, flexBasis: 0 })],
-        // [/^col-auto$/, () => ({ flex: '0 0 auto' })],
-
-        // [/^col-(\d+)$/, ([, d]) => ({
-        //     // padding: '0.3rem !important',
-        //     // 'text-align': 'end',
-        //     width: `${(Number(d) / 12) * 100}%`,
-        // })],
-
-        // [/^col-(\d+)$/, ([, d]) => ({
-        //     // padding: '0.3rem !important',
-        //     // 'text-align': 'end',
-        //     width: `${(Number(d) / 12) * 100}%`,
-        // })],
+    shortcuts: [
+        {
+            'input-group': 'grid-cols-12 grid gap-2 items-baseline m-2 [&_label]:md:text-right [&_input]:w-full [&_.p-select]:w-full [&_textarea]:w-full',
+        },
+        [/^row-(.*)$/, ([, c]) => `grid flex-row md:grid-cols-${c}`],
+        [/^input-group-col-(.*)$/, ([, c]) => `col-span-12 md:col-span-${c}`],
     ],
+    rules: [],
     presets: [
         presetUno(),
         presetAttributify(),
