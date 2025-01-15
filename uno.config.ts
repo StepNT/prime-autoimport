8cfbfa971b868791935d0607c2df2492bbe0251b
T660046@MacboookPro <T660046@tcrbank.com>
2025-01-15 13:08:53 +0700

    refactor: Simplify input-group-left styling for improved layout consistency

--- uno.config.ts
+++ uno.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
             'textarea': 'w-full',
 
             'input-group': 'grid-cols-12 grid gap-2 items-baseline m-2 [&_label]:md:text-right [&_label]:md:mr-2 [&_label]:font-semibold',
-            'input-group-left': 'grid-cols-12 grid gap-2 items-baseline m-2 [&_label]:md:text-left [&_label]:md:mr-2 [&_label]:font-semibold',
+            'input-group-left': 'input-group [&_label]:md:!text-left',
         },
         [/^row-(\d+)$/, ([, c]) => `grid flex-row md:grid-cols-${c}`],
         [/^col-(\d+)$/, ([, c]) => `col-span-12 md:col-span-${c}`],
