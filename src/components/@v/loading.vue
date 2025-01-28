ebf9c3d237390c76c060b684b01cc83233005e87
T660046 <T660046@TCRBANK.COM>
2025-01-28 14:01:08 +0700

    refactor: simplify loading component by removing unnecessary BlockUI wrapper

--- src/components/@v/loading.vue
+++ src/components/@v/loading.vue
@@ -3,21 +3,17 @@ const app = useAppStore()
 </script>
 
 <template>
-    <BlockUI>
-        <template #default>
-            <Dialog v-model:visible="app.isLoading" modal class="v-loading" pt:mask:class="backdrop-blur-sm">
-                <template #container>
-                    <div class="flex flex-col items-center justify-center">
-                        <ProgressSpinner
-                            style="width: 3.5rem; height: 3.5rem" stroke-width="3" fill="transparent"
-                            animation-duration="1s" aria-label="Custom ProgressSpinner"
-                        />
-                        <div text-md mt-3 animate-bounce text-gray-500 font-500>
-                            Loading...
-                        </div>
-                    </div>
-                </template>
-            </Dialog>
+    <Dialog v-model:visible="app.isLoading" modal class="v-loading">
+        <template #container>
+            <div class="flex flex-col items-center justify-center">
+                <ProgressSpinner
+                    style="width: 3.5rem; height: 3.5rem" stroke-width="3" fill="transparent"
+                    animation-duration="1s" aria-label="Custom ProgressSpinner"
+                />
+                <div text-md mt-3 animate-bounce text-gray-500 font-500>
+                    Loading...
+                </div>
+            </div>
         </template>
-    </BlockUI>
+    </Dialog>
 </template>
